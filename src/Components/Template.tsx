8a654714c7a720d7438d5dc541c49e50aa9fec1d
Liam Kronman <lkronman10@stuy.edu>
2024-01-07 17:15:50 -0500

    favicons

--- src/Components/Template.tsx
+++ src/Components/Template.tsx
@@ -38,14 +38,14 @@ const Template:React.FC<TemplateProps> = (
                 <title>{title}</title>
                 <meta name="description" content={description}/>
             </Helmet>
-            <header className="bg-purple-800 text-white">
+            <header className="bg-gray-100 text-purple-800">
                 <nav className="flex items-center justify-between flex-wrap p-6">
                     <div className="flex items-center flex-shrink-0 mr-6">
                         <img src={SAECoatOfArms} alt="SAE Logo" className="h-20 w-18" />
                         {/* <span className="font-semibold text-xl tracking-tight">MIT Sigma Alpha Epsilon</span> */}
                     </div>
                     <div className="block md:hidden">
-                        <button onClick={() => setIsMenuOpen(!isMenuOpen)} className="text-white hover:text-white focus:outline-none">
+                        <button onClick={() => setIsMenuOpen(!isMenuOpen)} className="text-purple-600 hover:text-purple-800 focus:outline-none">
                             <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d={isMenuOpen ? "M6 18L18 6M6 6l12 12" : "M4 6h16M4 12h16M4 18h16"} />
                             </svg>
@@ -57,7 +57,7 @@ const Template:React.FC<TemplateProps> = (
                                 <span
                                     key={link.text}
                                     onClick={() => handleNavigate(link.path)}
-                                    className="block mt-4 md:inline-block md:mt-0 md:text-left text-center mr-4 cursor-pointer font-bold hover:bg-purple-600 p-2 rounded transition-colors duration-300"
+                                    className="block mt-4 md:inline-block md:mt-0 md:text-left text-center mr-4 cursor-pointer font-bold uppercase hover:bg-amber-300 p-2 rounded transition-colors duration-300"
                                 >
                                     {link.text}
                                 </span>
